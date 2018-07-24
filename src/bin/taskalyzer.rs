d34f910b8a3bfa09ef700936ba401bc3d6c6edf8
Denis Lavrov <bahus.vel@gmail.com>
2018-07-24 23:56:27 +1200

    Native ptrace for taskalyzer

--- src/bin/taskalyzer.rs
+++ src/bin/taskalyzer.rs
@@ -1,21 +1,27 @@
 #![feature(plugin)]
 #![plugin(phf_macros)]
+#![feature(libc)]
 
 extern crate clap;
+extern crate libc;
+extern crate nix;
 extern crate phf;
 extern crate soundalyzer;
 
 mod syscallmap;
 
 use clap::{App, Arg};
+use nix::sys::ptrace;
+use nix::sys::wait::{waitpid, WaitStatus};
+use nix::unistd::Pid;
+use std::ptr;
 
 use soundalyzer::midi_backend::MIDIBackend;
 use soundalyzer::Backend;
 
 use std::error::Error;
 use std::io::{stdin, BufRead};
-
-type line_to_note = fn(&str) -> u64;
+use std::process::Command;
 
 fn syscall() -> Result<(), Box<Error>> {
     let mut backend = MIDIBackend::new()?;
@@ -26,6 +32,7 @@ fn syscall() -> Result<(), Box<Error>> {
     }
     Ok(())
 }
+
 fn main() {
     let matches = App::new("Taskalyzer")
         .version("0.0")
@@ -39,8 +46,37 @@ fn main() {
         )
         .get_matches();
 
-    match syscall() {
+    let trace: Result<(), Box<Error>> = {
+        move || {
+            let child = Command::new("sh").arg("-c").arg("echo hi").spawn()?;
+            let pid = Pid::from_raw(child.id() as i32);
+            ptrace::attach(pid)?;
+            let mut in_call = false;
+            loop {
+                let status = waitpid(pid, None)?;
+                match status {
+                    WaitStatus::PtraceSyscall(pid) => {
+                        in_call = !in_call;
+                        let a_reg = unsafe {
+                            ptrace::ptrace(
+                                ptrace::Request::PTRACE_PEEKUSER,
+                                pid,
+                                (8 * libc::RAX) as *mut nix::libc::c_void,
+                                ptr::null_mut(),
+                            )
+                        }?;
+                        ptrace::syscall(pid)?;
+                    }
+                    WaitStatus::Exited(_, _) => return Ok(()),
+                    exit => panic!("Unknown exit occured"),
+                }
+            }
+            Ok(())
+        }
+    }();
+
+    match trace {
         Ok(_) => (),
-        Err(err) => println!("Error: {}", err.description()),
+        Err(err) => println!("Error: {:?}", err),
     }
 }
